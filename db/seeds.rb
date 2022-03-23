6ff737092f3f3adb40fceb546bef1d48b9f740e2
Pikarin1121 <programerfish1206@gmail.com>
2022-03-23 13:20:42 +0000

    レイアウト調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create!(
+  [
+    {email: 'a@a', name: 'aaa', password: 'aaaaaa', profile_image_id: nil, birthday: '1990-1-1', introduction: '私の顔はない'},
+    {email: 'b@b', name: 'bbb', password: 'bbbbbb', profile_image_id: nil, birthday: '1992-2-2', introduction: '私はRails'},
+    {email: 'c@c', name: 'ccc', password: 'cccccc', profile_image_id: nil, birthday: '1993-3-3', introduction: '私は喫煙者'},
+    {email: 'd@d', name: 'ddd', password: 'dddddd', profile_image_id: nil, birthday: '1994-4-4', introduction: '私は着物です'}
+  ]
+)
+
+Holiday.create!(
+  [
+    {content: '娘と一緒に遊びました', image_id: nil, user_id: users[0].id},
+    {content: 'ファイト一発、あと少し', image_id: nil, user_id: users[1].id},
+    {content: '海で寝すぎると日焼けします', image_id: nil, user_id: users[2].id},
+    {content: 'この星でたった一つだけ', image_id: nil, user_id: users[3].id}
+  ]
+)
