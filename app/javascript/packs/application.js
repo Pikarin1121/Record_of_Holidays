473388465e5bc3d9a3c2a000d62c088b3410fe00
Pikarin1121 <programerfish1206@gmail.com>
2022-03-28 04:37:19 +0000

    [Add]アニメーションイベント

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,3 +29,13 @@ $(function() {
     arrows: true,
   });
 });
+
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 300);
+    event.preventDefault();
+  });
+});
+
